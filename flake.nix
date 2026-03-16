104e3bb957c7f99537cc4f8aac11b75f1a877ba5
cmacwill1 <charles.macwilliams@gmail.com>
2026-03-15 22:14:45 -0400

    env init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Go shell!";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+
+        packages = with pkgs; [
+          go
+        ];
+
+        shellHook = ''
+          echo "Go shell!"
+        '';
+      };
+    };
+}
